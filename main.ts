09d936d965702924a6ccc1d4b137466db7f79843
Florian Schwaiger <fschwaiger@gmail.com>
2024-08-27 22:43:18 +0200

    feat: initial draft, closes #4, #3 and starts #2

--- main.ts
+++ main.ts
@@ -1,81 +1,201 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Command, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+let noble = require('noble');
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+type Action = {
+	command?: string;
+	template?: string;
+	actionSet?: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+type ActionSet = {
+	BF: Action;
+	BR: Action;
+	BL: Action;
+	BB: Action;
+	TF: Action;
+	TR: Action;
+	TL: Action;
+	TB: Action;
+	__: Action;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const SIDES = [
+ '__',
+ 'BF',
+ 'BR',
+ 'BL',
+ 'BB',
+ 'TF',
+ 'TR',
+ 'TL',
+ 'TB',
+]
+
+function decodeSide(data: number): 'BF' | 'BR' | 'BL' | 'BB' | 'TF' | 'TR' | 'TL' | 'TB' | '__' {
+	switch (data) {
+		case 0x01:
+			return 'BF';
+		case 0x02:
+			return 'BR';
+		case 0x03:
+			return 'BL';
+		case 0x04:
+			return 'BB';
+		case 0x05:
+			return 'TF';
+		case 0x06:
+			return 'TR';
+		case 0x07:
+			return 'TL';
+		case 0x08:
+			return 'TB';
+		default:
+			return '__';
+	}
+}
+
+interface BluetoothTimeTrackerPluginSettings {
+	deviceName: string;
+	templateTargetFile: string;
+	activeActionSet: string;
+	actionSetsByName: { [key: string]: ActionSet }
+}
+
+const DEFAULT_SETTINGS: BluetoothTimeTrackerPluginSettings = {
+	deviceName: 'Timeular Tracker',
+	templateTargetFile: 'Daily/{{date}}.md',
+	activeActionSet: 'default',
+	actionSetsByName: {
+		'default': {
+			BF: {
+				template: '{{time}} BF'
+			},
+			BR: {
+				template: '{{time}} BR'
+			},
+			BL: {
+				template: '{{time}} BL'
+			},
+			BB: {
+				template: '{{time}} BB'
+			},
+			TF: {
+				template: '{{time}} TF'
+			},
+			TR: {
+				template: '{{time}} TR'
+			},
+			TL: {
+				template: '{{time}} TL'
+			},
+			TB: {
+				template: '{{time}} TB'
+			},
+			__: {
+				template: '{{time}} __'
+			}
+		}
+	}
+}
+
+const NOTIFICATION_CHARACTERISTIC_UUID = 'c7e70012_c847_11e6_8175_8c89a55d403c';
+
+export default class BluetoothTimeTrackerPlugin extends Plugin {
+	settings: BluetoothTimeTrackerPluginSettings;
+	state: 'disconnected' | 'connecting' | 'connected' = 'disconnected';
+	statusBarItemEl: HTMLElement;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+		const connectCommand = this.addCommand({
+			id: 'connect',
+			name: 'Connect Time Tracker',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				this.state = 'connecting';
+
+				noble.on('stateChange', (state: string) => {
+					if (state === 'poweredOn') {
+						noble.startScanning();
+						this.setStatus('Tracker: scanning...');
+					} else {
+						this.setStatus('Tracker: offline');
 					}
+				});
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				noble.on('discover', (peripheral: any) => {
+					if (peripheral.advertisement.localName === this.settings.deviceName) {
+						noble.stopScanning();
+						this.setStatus('Tracker: connecting...');
+						peripheral.connect((error: any) => {
+							if (error) {
+								this.setStatus('Tracker: connection error');
+								return;
+							}
+
+							peripheral.discoverAllServicesAndCharacteristics([], [NOTIFICATION_CHARACTERISTIC_UUID], (error: any, services: any, characteristics: any) => {
+								const notificationCharacteristic = characteristics.find((c: any) => c.uuid === NOTIFICATION_CHARACTERISTIC_UUID);
+								if (!notificationCharacteristic) {
+									return;
+								}
+
+								notificationCharacteristic.subscribe((error: any) => {
+									if (error) {
+										this.setStatus('Tracker: subscription error');
+										return;
+									}
+
+									this.setStatus('Tracker: connected');
+									this.state = 'connected';
+								});
+
+								notificationCharacteristic.on('data', (data: number) => {
+									let side = decodeSide(data);
+									this.setStatus(`Tracker: connected &lt;${side}&gt;`);
+
+									const actionSet = this.settings.actionSetsByName[this.settings.activeActionSet];
+									const action = actionSet[side];
+
+									if (action.template) {
+										new BluetoothTimeTrackerModal(this.app, `Template: ${action.template}`).open();
+									}
+									if (action.command) {
+										new BluetoothTimeTrackerModal(this.app, `Command: ${action.command}`).open();
+									}
+									if (action.actionSet) {
+										this.settings.activeActionSet = action.actionSet;
+										this.saveSettings();
+									}
+								});
+							});
+						});
+					}
+				});
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		const disconnectCommand = this.addCommand({
+			id: 'disconnect',
+			name: 'Disconnect Time Tracker',
+			callback: () => {
+				this.state = 'disconnected';
+				noble.stopScanning();
+				noble.removeAllListeners();
+				this.setStatus('Tracker: disconnected');
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		const ribbonIconEl = this.addRibbonIcon('diamond-minus', 'Time Tracker', (evt: MouseEvent) => {
+			if (this.state === 'disconnected') {
+				connectCommand.callback?.();
+			} else {
+				disconnectCommand.callback?.();
+			}
+		});
+
+		this.statusBarItemEl = this.addStatusBarItem();
+		this.addSettingTab(new BluetoothTimeTrackerSettingTab(this.app, this));
+		this.setStatus('Tracker: disconnected');
 	}
 
 	onunload() {
@@ -89,16 +209,20 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async setStatus(status: string) {
+		this.statusBarItemEl.setText(status);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class BluetoothTimeTrackerModal extends Modal {
+	constructor(app: App, public text: string) {
 		super(app);
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText(this.text);
 	}
 
 	onClose() {
@@ -107,10 +231,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class BluetoothTimeTrackerSettingTab extends PluginSettingTab {
+	plugin: BluetoothTimeTrackerPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: BluetoothTimeTrackerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +245,24 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Device Name')
+			.setDesc('The name of the time tracker device')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter the device name')
+				.setValue(this.plugin.settings.deviceName)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.deviceName = value;
+					await this.plugin.saveSettings();
+				}));
+		
+		new Setting(containerEl)
+			.setName('Template Target File')
+			.setDesc('The file to write the time entries to')
+			.addText(text => text
+				.setPlaceholder('Enter the template target file')
+				.setValue(this.plugin.settings.templateTargetFile)
+				.onChange(async (value) => {
+					this.plugin.settings.templateTargetFile = value;
 					await this.plugin.saveSettings();
 				}));
 	}
