84eff1a377f33c158fb4c789820d5735d4607a44
Florian Schwaiger <fschwaiger@gmail.com>
2024-08-28 11:33:54 +0200

    docs: ws.ts

--- ws.js
+++ ws.js
@@ -1,1 +1,4 @@
+// The noble library requires websocket bindings but they
+// are provided as native classes in modern browsers. This
+// module bind the native component to the noble library.
 module.exports = WebSocket;
